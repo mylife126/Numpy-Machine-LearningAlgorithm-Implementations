987a139b581c13ff70d58c41ab22293123103cad
mylife126 <shenxin6@hotmail.com>
2020-02-09 19:14:02 -0500

    added new models

--- Clustering/KNN.py
+++ Clustering/KNN.py
@@ -3,7 +3,6 @@ from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_classification
 
-
 class KNN(object):
 	def __init__(self, X, y, topK):
 		self.x = X
@@ -11,6 +10,11 @@ class KNN(object):
 		self.k = topK
 
 	def euclidian(self, pointA, pointB):
+
+		'''
+		pointA -- 1D vector, 1 by n
+		pointB -- 1D vector, 1 by n'''
+
 		assert pointA.shape == pointB.shape
 		dist = 0
 		for coord in range(len(pointA)):
@@ -18,20 +22,38 @@ class KNN(object):
 		return np.sqrt(dist)
 
 	def distance(self, testpoint, resultArray):
+		'''
+		This function calculate every test point with the training point, to get the distance. 
+		Then, the distance is ordered in the ascending order. What we want the is the top K training points as the associated point to the test point.
+
+		testpont    -- int[][]  in the shape of m by n
+		resultArray -- list<list<int[2]>>  x by m by 2, x means the x test point, m is the size of the train set. This would record each test point to every training points' [distance, idx]
+		'''
 		assert testpoint[0].shape == self.x[0].shape
+		#for each test point
 		for i in range(testpoint.shape[0]):
 			whichTestPoint = testpoint[i]
+			#we calculate the distance between each test point to every training point 
 			for j in range(self.x.shape[0]):
 				whichTrain = self.x[j]
+				#then for this test point, we record the distance and the associated traning point
 				resultArray[i].append([self.euclidian(whichTestPoint, whichTrain), j])
 
 
 	def predict(self, testpoint, mode):
+		'''
+		distanceArray -- list<list<int[2]>>  x by m by 2, x means the x test point, m is the size of the train set. This would record each test point to every training points' [distance, idx]
+		topK          -- list<list<int[2]>>  x by k by 2, the top k element for each test set
+		prediction    -- list<int>,          x by 1
+		'''
 		distanceArray = [[] for row in range(testpoint.shape[0])]
 
 		self.distance(testpoint, distanceArray)
+	
 		for eachSample in distanceArray:
+			#within each test sample, we sort the distance between it and all other train point
 			eachSample.sort(key = lambda x : x[0])
+
 		assert len(distanceArray[0]) == self.x.shape[0]
 
 		topK = []
@@ -42,16 +64,21 @@ class KNN(object):
 
 		assert topK.shape[0] == len(distanceArray) and topK.shape[1] == self.k and topK.shape[2] == 2
 
+		#if the testing task is the regression, then, for each testpoint, we calculate all the trainsamples associated ys, and take the average
 		if mode == 'regression':
 			prediction = []
+			#for each test sample
 			for sample in topK:
 				ypred = 0
+				#we take out all the distance and idx associated with it, and take out the associated y value within the same test point
+				#and the the average
 				for tuples in sample:
 					whichIdx = int(tuples[1])
 					ypred += self.y[whichIdx]
 				prediction.append(ypred / self.k)
 			return prediction
-
+		#if the test task is the classification, then for each test point, we take out the associated k train sample's y which is the class
+		#and take the mode cls as the cls predicted for this test point
 		if mode == 'classification':
 			from scipy import stats
 			prediction = []
@@ -66,16 +93,19 @@ class KNN(object):
 
 			return prediction
 
-	def plot(self, ypred, mode):
+	def plot(self, ypred, test):
 		fig = plt.figure(figsize = (8, 6))
 		plt.scatter(X[:,0], X[:,1], c=y)
-		plt.title("Dataset with 4 clusters")
+		plt.scatter(test[:,0], test[:,1], c = 'r')
+		plt.scatter(test[:,0], test[:,1], c = ypred)
+		# plt.title("Dataset with 4 clusters")
+
 		plt.xlabel("First feature")
 		plt.ylabel("Second feature")
+		plt.show()
 
 if __name__ == '__main__':
 	X, y = make_blobs(centers=6, n_samples=1000)
-	Xcls, ycls = make_classification(n_samples = 1000, n_features = 2, n_classes = 2, n_informative = 2, n_redundant = 0)
 	fig = plt.figure(figsize=(8,6))
 	plt.scatter(X[:,0], X[:,1], c=y)
 	plt.title("Dataset with 4 clusters")
@@ -86,9 +116,8 @@ if __name__ == '__main__':
 
 	knn = KNN(X, y, 3)
 	samplePoint = np.array([[2,3],[4,5],[10,24]])
-	yPred = knn.predict(samplePoint, "classification")
-	print(y)
-	print(yPred)
+	yPred = knn.predict(samplePoint, "regression")
+	knn.plot(yPred, samplePoint)
 
 
 
---
+++ linearModel/ridgeRegression.py
@@ -0,0 +1,90 @@
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rd
+from sklearn.datasets import make_regression
+
+
+class ridgeRegression():
+	def __init__(self, alpha, intercept):
+		'''
+		using normal equation to fit a ridge regression problem
+
+		W = (AT@A + aI)^-1 AT@Y
+		   n by n @ n * m @ m by 1 => n by 1 
+
+		
+		self.theta    -- int[N][N] the weight for the model X@theta  
+		self.intercep -- boolean  decide if wanna include the interception or not
+		'''
+		self.a = alpha
+		self.intercept = intercept
+		self.mRows = None
+		self.nFeatures = None
+		self.theta = None
+
+	def addOneToFeature(self, X):
+		ones = np.ones(self.mRows)    #dim = (m,)
+		ones = np.expand_dims(ones, 1) #give an extra dimention along the col direction
+		print(ones.shape)
+		X = np.concatenate((ones, X), 1) #concatenate along the col direction
+
+	def fit(self, X, y):
+		'''
+		X   -- the training data in the shape of m by n 
+		y   -- the training label in the shape of m by 1
+
+		return:
+		weights -- in the shape of n by 1
+		'''
+		#if we want to include an extra intercept， we need to introduce an extra column with all the ones to the X
+		self.mRows = X.shape[0]
+		if self.intercept:
+			self.addOneToFeature(X)
+
+		
+		self.nFeatures = X.shape[1]			
+		
+		firstMatrix =np.linalg.inv((X.T @ X + self.a * np.eye(self.nFeatures)))
+		self.theta = firstMatrix @ X.T @ y
+
+	def predict(self, testData):
+		'''
+		testData -- in the shape of k by n
+
+		return:
+		yPred    -- in the shape of k by 1
+		'''
+
+		if self.intercept:
+			self.addOneToFeature(testData)
+
+		return testData @ self.theta
+
+	def plot(self, X, y, ypred):
+		y = np.expand_dims(y, 1)
+		fig = plt.figure()
+		ax = plt.axes(projection='3d')
+		ax.scatter3D(X[:,0], X[:,1], y, label = 'groundTruth')
+		
+		ypred = np.expand_dims(ypred, 1)
+		ax.scatter3D(X[:,0], X[:,1], ypred, marker = '^', s = 100, label = 'predict')
+		ax.set_xlabel('x')
+		ax.set_ylabel('y')
+		ax.set_zlabel('Value')
+		ax.legend()
+
+		plt.show()
+
+if __name__ == '__main__':
+	X, y = make_regression(n_samples = 100, n_features = 2)
+	print(X.shape)
+
+
+	ridge = ridgeRegression(2, True)
+	ridge.fit(X,y)
+	ypred = ridge.predict(X)
+
+	ridge.plot(X, y, ypred)
+
+
